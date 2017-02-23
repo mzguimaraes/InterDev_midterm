0a92793a1ade3d0f56ba4d92ccbfd293796f4b36
Marcus Guimaraes <mguimaraes42@gmail.com>
2017-02-22 23:50:27 -0500

    added materials and scripts for shirts and fixtures

---
+++ Assets/Scripts/Fixture.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fixture : MonoBehaviour {
+
+	public enum shirtColor {
+		Red,
+		Black,
+		Blue
+	}
+
+	public shirtColor color;
+	public int maxShirts = 3;
+	private int currShirts = maxShirts;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Shirt.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shirt : MonoBehaviour {
+	//empty tag class
+}
