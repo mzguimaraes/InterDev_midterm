096cedbd586bb30541db36a29f864828ef78d038
Marcus Guimaraes <mguimaraes42@gmail.com>
2017-02-22 22:21:18 -0500

    guest gizmos implemented

--- Assets/Scripts/GuestBehavior.cs
+++ Assets/Scripts/GuestBehavior.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEditor;
+
 public class GuestBehavior : MonoBehaviour {
 	public List<GuestWaypoint> waypoints;
 	private Queue<GuestWaypoint> wps;
@@ -9,7 +11,7 @@ public class GuestBehavior : MonoBehaviour {
 	private GuestWaypoint destination;
 	public float wpRadius = 1f;
 
-	public float speed = 10f;
+	private float speed = 7f; //randomized in Start()
 
 	void Start () {
 		wps = new Queue<GuestWaypoint>();
@@ -18,6 +20,8 @@ public class GuestBehavior : MonoBehaviour {
 		}
 
 		destination = wps.Dequeue();
+
+		speed *= Random.Range(0.5f, 1.5f);
 	}
 
 	// Update is called once per frame
@@ -37,4 +41,31 @@ public class GuestBehavior : MonoBehaviour {
 		transform.position += (destination.transform.position - transform.position).normalized * speed * Time.deltaTime;
 
 	}
+
+	void OnDrawGizmos() {
+		//show path if this guest is selected
+
+		if (Selection.Contains(gameObject)) {
+			
+			List<GuestWaypoint> wpsCopy; //copy into a list so we can access items freely
+			if (Application.isPlaying) { // if the game hasn't started, wps is null and cannot be used to init wpsCopy
+				wpsCopy = new List<GuestWaypoint>(wps); 
+			}
+			else { //so we can just use waypoints
+				wpsCopy = waypoints;
+			}
+
+			Gizmos.color = Color.cyan;
+
+			Gizmos.DrawLine(transform.position, destination.transform.position);
+			if (wpsCopy.Count > 0) {
+				Gizmos.DrawLine(destination.transform.position, wpsCopy[0].transform.position);
+
+				for(int i = 0; i < wpsCopy.Count - 1; i ++) {
+					Gizmos.DrawLine(wpsCopy[i].transform.position, wpsCopy[i + 1].transform.position);
+				}
+			}
+		}
+	}
+
 }
